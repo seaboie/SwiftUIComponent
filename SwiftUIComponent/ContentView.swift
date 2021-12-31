524497e91512eb83cc513d5df5e5cd5f61efac95
kritbovorn <kritbovorn@gmail.com>
2021-12-31 10:54:57 +0700

    Initial Commit

---
+++ SwiftUIComponent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIComponent
+//
+//  Created by Kritbovorn Taweeyossak on 31/12/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
