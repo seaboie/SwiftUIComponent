87754c7d26ed06fb3fef4a03a561011a2cdce9a7
kritbovorn <kritbovorn@gmail.com>
2021-12-31 10:54:57 +0700

    Initial Commit

---
+++ SwiftUIComponent/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIComponent
+//
+//  Created by Kritbovorn Taweeyossak on 31/12/2564 BE.
+//
+//  https://www.youtube.com/watch?v=S5e1eXL8Vpk&list=PLwvDm4VfkdpiagxAXCT33Rkwnc5IVhTar&index=2
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIComponent/SwiftUIComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentApp.swift
+//  SwiftUIComponent
+//
+//  Created by Kritbovorn Taweeyossak on 31/12/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
