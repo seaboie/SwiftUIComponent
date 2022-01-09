da4108e90768e9150fed078dee0a78c2a144d03d
kritbovorn <kritbovorn@gmail.com>
2022-01-09 20:19:35 +0700

    Finish

--- SwiftUIComponent/ContentView.swift
+++ SwiftUIComponent/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var angle: Angle = Angle(degrees: 0.0)
+    
+    var rotateGesture: some Gesture {
+        RotationGesture()
+            .onChanged { angleValue in
+                angle = angleValue
+            }
+            .onEnded { angleValue in
+                withAnimation(.spring()) {
+                    angle = Angle(degrees: 0.0)
+                }
+            }
+    }
+    
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundColor(Color(.systemBackground))
+            .padding(30)
+            .background(Color(.systemBlue))
+            .cornerRadius(12)
+            .rotationEffect(angle)
+            .gesture(rotateGesture)
     }
 }
 
