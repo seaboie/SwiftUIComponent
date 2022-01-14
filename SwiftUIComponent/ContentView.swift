417e1de050013fcb3d4cc2d35130ca65631a9ce6
kritbovorn <kritbovorn@gmail.com>
2022-01-14 18:22:58 +0700

    Finish

--- SwiftUIComponent/ContentView.swift
+++ SwiftUIComponent/ContentView.swift
@@ -9,9 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var textFieldText: String = ""
+    @State private var scrollToIndex: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            TextField("Enter a number here...", text: $textFieldText)
+                .padding(.horizontal)
+                .frame(height: 55)
+                .border(Color.gray)
+                .padding(.horizontal)
+                .keyboardType(UIKit.UIKeyboardType.numberPad)
+            
+            Button {
+                    
+                    if let index = Int(textFieldText) {
+                        scrollToIndex = index
+                    }
+        
+            } label: {
+                Text("Scroll now!!!")
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+
+                    ForEach(0..<50) { index in
+                        Text("This is item # \(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white.cornerRadius(12))
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { newValue in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(newValue, anchor: .top)
+                        }
+                    }
+                }
+                
+            }
+        }
     }
 }
 
