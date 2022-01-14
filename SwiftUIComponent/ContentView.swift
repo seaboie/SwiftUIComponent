8bde1528cadbac228a33094401fec7d2c4546a5a
kritbovorn <kritbovorn@gmail.com>
2022-01-14 19:50:37 +0700

    finish

--- SwiftUIComponent/ContentView.swift
+++ SwiftUIComponent/ContentView.swift
@@ -10,9 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ScrollView(Axis.Set.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    
+                    GeometryReader { geometry in
+                        
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(Angle(degrees: getPercentage(geo: geometry) * 40), axis: (x: 0, y: 1, z: 0))
+                        
+                    }
+                    .frame(width: 300, height: 250)
+                }
+                .padding()
+            }
+        }
+        
     }
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        
+        let maxDistance = UIScreen.main.bounds.size.width / 2
+        print("### MaxDistance = \(maxDistance)")
+        let currentX = geo.frame(in: .global).midX
+        print("@@@ Current X: = \(currentX)")
+        return Double(1 - (currentX / maxDistance))
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
