ca7e8de04f9e181975974abe747d3ef2e1fdc4fd
kritbovorn <kritbovorn@gmail.com>
2022-01-15 16:19:18 +0700

    Finish

--- SwiftUIComponent/ContentView.swift
+++ SwiftUIComponent/ContentView.swift
@@ -9,9 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var rating: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        starView
+            .overlay(
+                overlayView
+                    .mask(starView)
+            )
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(Color(.systemYellow))
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starView: some View {
+        HStack {
+            
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(Color(.systemGray))
+                    .onTapGesture {
+                        withAnimation(.spring()) {
+                            rating = index
+                        }
+                    }
+            }
+            
+        }
     }
 }
 
